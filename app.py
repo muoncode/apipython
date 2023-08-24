779a2b43f688a03497cb1b699232ace0233ab8c3
muoncode <save230421@gmail.com>
2023-08-25 00:12:37 +0700

    add data with url

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Flask!"
+    name = request.args.get('name')
+    return f"Hello, {name}!"
 
 if __name__ == '__main__':
     app.run()
