552a8338f740b68288a7518106abb4bb5559c2b0
muoncode <save230421@gmail.com>
2023-08-25 00:14:08 +0700

    add html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get('name')
-    return f"Hello, {name}!"
+    return f"<h1>Hello, {name}!</h1>"
 
 if __name__ == '__main__':
     app.run()
